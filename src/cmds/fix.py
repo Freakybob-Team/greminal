67015b475631daa2965724ffe8adeff4ab0e69ff
Wish <wishyt@proton.me>
2025-10-22 17:35:17 -0500

    add fix command fully

--- src/cmds/fix.py
+++ src/cmds/fix.py
@@ -1,9 +1,14 @@
 import urllib.request
 import json
+import os
 print("This file uses Internet, and downloads from github.com")
 input("Run CTRL+C to exit or press enter to continue.")
 with urllib.request.urlopen("https://freakybob-team.github.io/greminal/data/cmds.json") as url:
     data = json.loads(url.read().decode())
 for item in data:
+    if os.path.exists("cmds/"):
+        print("cmds/ found; not creating")
+    else:
+        os.mkdir("cmds/")
     urllib.request.urlretrieve("https://freakybob-team.github.io/greminal/src/cmds/" + item["name"] + ".py", "cmds/" + item["name"]  + ".py")
 print("Commands downloaded!")

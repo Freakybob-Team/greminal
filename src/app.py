08fe6d72747766a2b06773aef13b356d434fa477
Wish <wishyt@proton.me>
2025-10-22 17:50:06 -0500

    add goodbye message if you exit

--- src/app.py
+++ src/app.py
@@ -1,32 +1,35 @@
-import os
-import urllib.request
-import subprocess
-try:
-    import cmds.usredit as usredit
-    import cmds.osl as osl
-except:
-    print("A command couldn't be imported. Commands will not work other than fix.")
-print("Greminal - The Greg Terminal")
-print("By Freakybob Team - run help for list of commands")
-print("Run osl for licenses.")
-print("Set username with usredit")
-print("Commands are stored in the cmds folder. if not found, run fix.")
-username = "admin"
-if os.path.exists("user.txt"):
-    with open("user.txt", "r") as file:
-        username = file.read()
-def cmdInput():
-    global cmd
-    cmd = input(f"{username}@greminal:$ ")
-cmdInput()
-if cmd == "usredit":
-    usredit.cmd()
-if cmd == "osl":
-    osl.cmd()
+try:    
+    import os
+    import urllib.request
+    import subprocess
+    try:
+        import cmds.usredit as usredit
+        import cmds.osl as osl
+    except:
+        print("A command couldn't be imported. Commands will not work other than fix.")
+    print("Greminal - The Greg Terminal")
+    print("By Freakybob Team - run help for list of commands")
+    print("Run osl for licenses.")
+    print("Set username with usredit")
+    print("Commands are stored in the cmds folder. if not found, run fix.")
+    username = "admin"
+    if os.path.exists("user.txt"):
+        with open("user.txt", "r") as file:
+            username = file.read()
+    def cmdInput():
+        global cmd
+        cmd = input(f"{username}@greminal:$ ")
     cmdInput()
-if cmd == "fix":
-    print("This command requires internet, and downloads from github.com.")
-    input("Run CTRL+C to exit or press enter to continue.")
-    urllib.request.urlretrieve("https://freakybob-team.github.io/greminal/src/cmds/fix.py", "fix.py")
-    print("fix.py downloaded, now running")
-    subprocess.call(['python', 'fix.py'])
+    if cmd == "usredit":
+        usredit.cmd()
+    if cmd == "osl":
+        osl.cmd()
+        cmdInput()
+    if cmd == "fix":
+        print("This command requires internet, and downloads from github.com.")
+        input("Run CTRL+C to exit or press enter to continue.")
+        urllib.request.urlretrieve("https://freakybob-team.github.io/greminal/src/cmds/fix.py", "fix.py")
+        print("fix.py downloaded, now running")
+        subprocess.call(['python', 'fix.py'])
+except:
+    print(f"\nThere was either an error or you exited Greminal yourself. Hope to see you back soon, {username}! :)")

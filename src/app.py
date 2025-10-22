367829ef4d5f5bfed55cb218fe68bb5a8518480e
Wish <wishyt@proton.me>
2025-10-22 17:53:35 -0500

    change fix to update

--- src/app.py
+++ src/app.py
@@ -16,7 +16,7 @@ try:
     print("By Freakybob Team - run help for list of commands")
     print("Run osl for licenses.")
     print("Set username with usredit")
-    print("Commands are stored in the cmds folder. if not found, run fix.")
+    print("Commands are stored in the cmds folder. if not found, run update.")
     username = "admin"
     if os.path.exists("user.txt"):
         with open("user.txt", "r") as file:
@@ -30,11 +30,11 @@ try:
     if cmd == "osl":
         osl.cmd()
         cmdInput()
-    if cmd == "fix":
+    if cmd == "update":
         print("This command requires internet, and downloads from github.com.")
         input("Run CTRL+C to exit or press enter to continue.")
-        urllib.request.urlretrieve("https://freakybob-team.github.io/greminal/src/cmds/fix.py", "fix.py")
+        urllib.request.urlretrieve("https://freakybob-team.github.io/greminal/src/cmds/fix.py", "update.py")
         print("fix.py downloaded, now running")
-        subprocess.call(['python', 'fix.py'])
+        subprocess.call(['python', 'update.py'])
 except:
     print(f"\nThere was either an error or you exited Greminal yourself. Hope to see you back soon, {username}! :)")
